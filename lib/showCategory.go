ccd3a05adc9247144c5c9c7942cc20d758a313bf
almarufh_ <hidayatmaruf99@gmail.com>
2026-07-10 15:36:48 +0800

    feat: create template all menu

---
+++ lib/showCategory.go
@@ -0,0 +1,46 @@
+package lib
+
+import (
+	"authenticatiion-flow/database"
+	"authenticatiion-flow/utils"
+	"fmt"
+	"os"
+)
+
+func ShowCategory () {
+	utils.Clear()
+	var input string
+	fmt.Printf("\n=======================================")
+	fmt.Printf("\nList Menu %s", database.Project)
+	fmt.Printf("\n=======================================\n\n")
+	fmt.Println("[1] Promotion")
+	fmt.Println("[2] Beverage")
+	fmt.Println("[3] Chicken")
+	fmt.Println("[4] Bowl/Burger")
+	fmt.Println("\n\n--------------------------------------")
+	fmt.Println("[0] Exit")
+	fmt.Println("[00] Back")
+	fmt.Println("\n\n=======================================\n")
+	fmt.Printf("Pilih Kategori (1-4) :  ")
+	fmt.Scanf("%s", &input)
+	
+	switch input {
+	case "1":
+		fmt.Println("Masih dalam pengembangan")
+	case "2":
+		fmt.Println("Masih dalam pengembangan")
+		return
+	case "3":
+		fmt.Println("Masih dalam pengembangan")
+		return
+	case "4":
+		fmt.Println("Masih dalam pengembangan")
+		return
+	case "0":
+		os.Exit(1)
+	case "00":
+		return
+	default:
+		ShowCategory()
+	}
+}
