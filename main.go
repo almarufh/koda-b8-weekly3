e90ac298abef8da0820e9078524a59a52d56a233
almarufh_ <hidayatmaruf99@gmail.com>
2026-07-10 17:38:48 +0800

    feat: use go rutin for get product and load when data not ready

--- main.go
+++ main.go
@@ -1,7 +1,35 @@
 package main
 
-import "authenticatiion-flow/lib"
+import (
+	"authenticatiion-flow/database"
+	"authenticatiion-flow/lib"
+	"authenticatiion-flow/utils"
+	"fmt"
+	"sync"
+	"time"
+)
 
 func main() {
+	var wait sync.WaitGroup
+	signal := make(chan int)
+	wait.Add(1)
+	go utils.GetProducts(&wait, &signal)
+	defer wait.Wait()
+	i := 0
+	for i < 1 {
+		utils.Clear()
+		fmt.Println("Loading .")
+		time.Sleep(time.Duration(1) * time.Second)
+		utils.Clear()
+		fmt.Println("Loading . .")
+		time.Sleep(time.Duration(1) * time.Second)
+		utils.Clear()
+		fmt.Println("Loading . . .")
+		time.Sleep(time.Duration(1) * time.Second)
+		if <-signal == 1 {
+			i++
+		}
+	}
+	fmt.Println(database.GetProducts())
 	lib.Menu()
 }
