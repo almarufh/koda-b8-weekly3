edafb7923a76ca8072aca6193d04fbafd54d780b
almarufh_ <hidayatmaruf99@gmail.com>
2026-07-11 15:22:04 +0800

    feat: func name Actived

---
+++ utils/nameActive.go
@@ -0,0 +1,17 @@
+package utils
+
+import (
+	"authenticatiion-flow/database"
+)
+
+func NameActived() string {
+	getActived := database.GetActived()
+	actived := *getActived
+	var name string
+	for i := range actived {
+		if actived[i].Status == true {
+			name = actived[i].Name
+		}
+	}
+	return name
+}
