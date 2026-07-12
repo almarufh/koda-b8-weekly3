4e4d3e4892a1f295bbf83a96de9fb4e2134dcf98
almarufh_ <hidayatmaruf99@gmail.com>
2026-07-12 16:04:28 +0800

    feat: add paramaeter for show full name and username

--- lib/createpassword.go
+++ lib/createpassword.go
@@ -6,31 +6,36 @@ import (
 	"regexp"
 )
 
-func CreatePassword() string {
+func CreatePassword(name string, username string) string {
 	var password1 string
 	var password2 string
 
-	utils.Clear()
-	fmt.Printf("Create New PIN (4 digits):  ")
-	fmt.Scanln(&password1)
-
-	value, _ := regexp.MatchString("^[0-9]{4}$", password1)
-
-	if !value {
+	for {
 		utils.Clear()
-		fmt.Printf("PIN must be exactly 4 digits of numbers!!!\n\nPress ENTER to retry... ")
-		fmt.Scanln()
-		return CreatePassword()
-	}
+		fmt.Printf("Full Name : %s\n", name)
+		fmt.Printf("Username  : %s\n", username)
+		fmt.Printf("Create New PIN (4 digits):  ")
+		fmt.Scanln(&password1)
 
-	fmt.Printf("Confirm New PIN:  ")
-	fmt.Scanln(&password2)
+		value, _ := regexp.MatchString("^[0-9]{4}$", password1)
 
-	if password1 != password2 {
-		utils.Clear()
-		fmt.Printf("Wrong confirm password!!!\n\nPress ENTER to retry! ")
-		fmt.Scanln()
-		return CreatePassword()
+		if !value {
+			utils.Clear()
+			fmt.Printf("PIN must be exactly 4 digits of numbers!!!\n\nPress ENTER to retry... ")
+			fmt.Scanln()
+			continue
+		}
+
+		fmt.Printf("Confirm New PIN:  ")
+		fmt.Scanln(&password2)
+
+		if password1 == password2 {
+			return password1
+		} else {
+			utils.Clear()
+			fmt.Printf("Wrong confirm password!!!\n\nPress ENTER to retry! ")
+			fmt.Scanln()
+			continue
+		}
 	}
-	return password1
 }
