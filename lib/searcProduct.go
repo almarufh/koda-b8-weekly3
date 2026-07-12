6976f2eab7855990af05ef54515a7edd2b5e533d
almarufh_ <hidayatmaruf99@gmail.com>
2026-07-12 10:15:27 +0800

    feat: func search product

---
+++ lib/searcProduct.go
@@ -0,0 +1,70 @@
+package lib
+
+import (
+	"authenticatiion-flow/database"
+	"authenticatiion-flow/utils"
+	"fmt"
+	"strings"
+)
+
+func SearchProduct(search string) {
+	getCategory := database.GetCategory()
+	newCategory := []database.Category{}
+	products := database.GetProducts()
+	urut := 0
+
+	for _, results := range *products {
+		res := strings.Contains(strings.ToLower(results.Name), strings.ToLower(search))
+		if res == true {
+			urut += 1
+			product := database.Category{
+				Urut:        urut,
+				Id:          results.Id,
+				Name:        results.Name,
+				Description: results.Description,
+				Price:       results.Price,
+				Stock:       results.Stock,
+			}
+			newCategory = append(newCategory, product)
+		}
+	}
+
+	*getCategory = newCategory
+
+	for {
+		utils.Clear()
+		var input int
+		qty := len(*getCategory)
+		fmt.Printf(`*keyword "%s" ditemukan %d Products`, search, qty)
+		fmt.Printf("\n\n")
+		for i, results := range *getCategory {
+			if i <= 8 {
+				fmt.Printf("[%d]  %s <----> [Rp%d]\n", i+1, results.Name, results.Price)
+			} else {
+				fmt.Printf("[%d] %s <----> [Rp%d]\n", i+1, results.Name, results.Price)
+			}
+		}
+
+		fmt.Printf("\n\n--------------------------\n[0] Back\n\nChose menu (1-%d): ", qty)
+
+		_, err := fmt.Scanln(&input)
+
+		if err != nil {
+			utils.WrongInput()
+			continue
+		}
+
+		if input > 0 && input <= qty {
+			Order(input)
+			continue
+		}
+
+		switch input {
+		case 0:
+			return
+		default:
+			utils.WrongInput()
+			continue
+		}
+	}
+}
