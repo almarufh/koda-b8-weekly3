10d7af902f9beca32832ddfdd6871c758aa5b09b
almarufh_ <hidayatmaruf99@gmail.com>
2026-07-12 15:43:26 +0800

    fix: use lop for no recursive

--- lib/register.go
+++ lib/register.go
@@ -11,48 +11,51 @@ func Register(users *[]database.Users) {
 	var last string
 	var email string
 
-	utils.Clear()
-	fmt.Printf("\n\n--- REGISTER ---\n\nFirst Name :  ")
-	fmt.Scanf("%s", &first)
+	for {
+		utils.Clear()
+		fmt.Printf("\n\n--- REGISTER ---\n\nFirst Name :  ")
+		fmt.Scanf("%s", &first)
 
-	fmt.Printf("Last Name :  ")
-	fmt.Scanf("%s", &last)
+		fmt.Printf("Last Name :  ")
+		fmt.Scanf("%s", &last)
 
-	fmt.Printf("Username :  ")
-	fmt.Scanf("%s", &email)
+		fmt.Printf("Username :  ")
+		fmt.Scanf("%s", &email)
 
-	password := CreatePassword()
+		password := CreatePassword()
 
-	newUser := database.Users{
-		First:    first,
-		Last:     last,
-		Email:    email,
-		Password: utils.Encrypted(password),
-	}
-
-	defer func() {
-		if r := recover(); r != nil {
-			fmt.Printf("\nEmail belum terdaftar : %v\n", r)
+		newUser := database.Users{
+			First:    first,
+			Last:     last,
+			Email:    email,
+			Password: utils.Encrypted(password),
 		}
-	}()
 
-	utils.Clear()
-	var confirmAccept string
-	fmt.Printf("\n\nIs it true?\n\n")
-	fmt.Println("First Name : ", first)
-	fmt.Println("Last Name : ", last)
-	fmt.Println("Username : ", email)
-	fmt.Printf("\n\nContinue (y/n) :  ")
-	fmt.Scanf("%s", &confirmAccept)
-	switch confirmAccept {
-	case "y":
-		*users = append(*users, newUser)
-		fmt.Printf("\n\nRegister success, press enter to back..")
-		fmt.Scanf("\n")
-		Menu()
-	case "n":
-		Register(users)
-	default:
-		Menu()
+		defer func() {
+			if r := recover(); r != nil {
+				fmt.Printf("\nEmail belum terdaftar : %v\n", r)
+			}
+		}()
+
+		utils.Clear()
+		var confirmAccept string
+		fmt.Printf("\n\nIs it true?\n\n")
+		fmt.Println("First Name : ", first)
+		fmt.Println("Last Name : ", last)
+		fmt.Println("Username : ", email)
+		fmt.Printf("\n\nContinue (y/n) :  ")
+		fmt.Scanf("%s", &confirmAccept)
+		switch confirmAccept {
+		case "y":
+			*users = append(*users, newUser)
+			fmt.Printf("\n\nRegister success, press enter to back..")
+			fmt.Scanf("\n")
+			return
+		case "n":
+			continue
+		default:
+			utils.WrongInput()
+			continue
+		}
 	}
 }
