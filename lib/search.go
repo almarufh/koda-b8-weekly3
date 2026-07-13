fc7b4e88a066180e03fdf77c72b0839f6ad86998
almarufh_ <hidayatmaruf99@gmail.com>
2026-07-13 09:29:29 +0800

    fix: bug not found

--- lib/search.go
+++ lib/search.go
@@ -3,12 +3,32 @@ package lib
 import (
 	"authenticatiion-flow/utils"
 	"fmt"
+	"regexp"
+	"strings"
 )
 
 func Search() {
-	utils.Clear()
-	var search string
-	fmt.Printf("\nSearch what you need products :  ")
-	fmt.Scanf("%s", &search)
-	SearchProduct(search)
+	for {
+		var search string
+		utils.Clear()
+		fmt.Printf(`Input "Back" and press ENTER for back`)
+		fmt.Printf("\n\n")
+		fmt.Printf("\nSearch what you need products :  ")
+		fmt.Scanln(&search)
+
+		if strings.ToLower(search) == "back" {
+			return
+		}
+
+		value, _ := regexp.MatchString("^[a-zA-Z]{3,}$", search)
+
+		if !value {
+			utils.Clear()
+			fmt.Printf("Keyword must min 4 character !!!\n\nPress ENTER to retry... ")
+			fmt.Scanln()
+			continue
+		}
+
+		SearchProduct(search)
+	}
 }
